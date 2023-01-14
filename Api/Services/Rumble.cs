866ae574cce162610551e0ce6f39775d570637e4
David Thiessen <davidt@melbappdev.com>
2023-01-14 16:46:17 +1100

    updates

---
+++ Api/Services/Rumble.cs
@@ -0,0 +1,107 @@
+﻿using AngleSharp.Dom;
+using BlazorApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Api.Services
+{
+    public interface IRumble
+    {
+        IEnumerable<XElement> CreateElements(IEnumerable<Item> items);
+        Task<Channel> Get(string channel);
+        //Task<Entries> GetHtml(string channel);
+    }
+
+    public class Rumble : IRumble
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public Rumble(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Channel> Get(string query)
+        {
+            var entries = await GetHtml(query);
+
+            var items = new List<Item>();
+
+            foreach (var entry in entries.items)
+            {
+                var item = new Item();
+
+                item.Title = WebUtility.HtmlDecode(entry.GetElementsByClassName("video-item--title")?.FirstOrDefault()?.Html() ?? string.Empty);
+                item.Url = WebUtility.UrlDecode("https://rumble.com/" + entry.GetElementsByClassName("video-item--a")?.FirstOrDefault()?.GetAttribute("href"));
+                item.img = WebUtility.UrlDecode(entry.GetElementsByClassName("video-item--img")?.FirstOrDefault()?.GetAttribute("src") ?? string.Empty);
+
+                var dt = entry.GetElementsByClassName("video-item--meta video-item--time")?.FirstOrDefault()?.GetAttribute("datetime");
+
+                item.PubDate = DateTimeOffset.Parse(dt);
+
+                items.Add(item);
+            }
+
+            var channel = new Channel();
+            channel.Title = query;
+            channel.Link = query;
+            channel.Items = items;
+            channel.Type = entries.type;
+
+            return channel;
+        }
+
+        public async Task<Entries> GetHtml(string channel)
+        {
+            var type = RumbleType.Channel;
+
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync($"https://rumble.com/c/{channel}");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Gone || response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                type = RumbleType.User;
+                response = await client.GetAsync($"https://rumble.com/user/{channel}");
+            }
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            var parser = new AngleSharp.Html.Parser.HtmlParser();
+            var doc = parser.ParseDocument(html);
+
+            var entries = doc.GetElementsByClassName("video-listing-entry");
+
+            return new Entries(type, entries);
+        }
+
+        public IEnumerable<XElement> CreateElements(IEnumerable<Item> items)
+        {
+            List<XElement> list = new List<XElement>();
+
+            foreach (var item in items)
+            {
+
+                XElement itemElement = new XElement("item",
+                                         new XElement("id", item.Url),
+                                         new XElement("title", item.Title),
+                                         new XElement("link", item.Url),
+                                         new XElement("image", item.img),
+                                         new XElement("pubDate", item.PubDate)
+                );
+
+                list.Add(itemElement);
+            }
+            return list;
+        }
+
+        public record Entries(RumbleType type, IHtmlCollection<IElement> items);
+
+    }
+
+}
---
+++ Shared/Rumble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorApp.Shared
+{
+    public enum RumbleType
+    {
+        User,
+        Channel
+    }
+
+    public class Channel
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public RumbleType Type { get; set; }
+        public IEnumerable<Item> Items { get; set; } = Enumerable.Empty<Item>();
+    }
+
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string img { get; set; }
+        public DateTimeOffset PubDate { get; set; }
+    }
+}
